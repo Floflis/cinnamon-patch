272d46da00aff26bcb1ee4b53589a09f024a2a8c
Daniella Mesquita <mesquitadaniella@proton.me>
2022-12-31 23:38:27 -0300

    More detailed error message, with diffing versions

--- patch.sh
+++ patch.sh
@@ -8,4 +8,7 @@ then
 #   sudo cp -f files/usr/share/cinnamon/cinnamon-settings-users/cinnamon-settings-users.py /usr/share/cinnamon/cinnamon-settings-users/
 else
    echo "Cannot patch Cinnamon as the version is different than expected by the patcher"
+   echo "Expected/supported version: Cinnamon $(cat currentversion.txt)"
+   echo "Your current version: $(cinnamon --version)"
+   echo "Don't worry, this issue is probably from our side. If you have an up-to-date Cinnamon version, our patcher will soon keep up with the current."
 fi
