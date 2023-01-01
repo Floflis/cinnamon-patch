92422f7392348c8aadc783411d20d81877954b58
Daniella Mesquita <mesquitadaniella@proton.me>
2022-12-31 23:34:06 -0300

    Move to latest version of Cinnamon and temporarily deactivate some items

--- patch.sh
+++ patch.sh
@@ -4,8 +4,8 @@ if [ "$(cinnamon --version)" = "Cinnamon $(cat currentversion.txt)" ];
 then
    echo "Patching Cinnamon..."
    sudo cp -f js/ui/panel.js /usr/share/cinnamon/js/ui/
-   sudo cp -f files/usr/share/cinnamon/cinnamon-settings/cinnamon-settings.py /usr/share/cinnamon/cinnamon-settings/
-   sudo cp -f files/usr/share/cinnamon/cinnamon-settings-users/cinnamon-settings-users.py /usr/share/cinnamon/cinnamon-settings-users/
+#   sudo cp -f files/usr/share/cinnamon/cinnamon-settings/cinnamon-settings.py /usr/share/cinnamon/cinnamon-settings/
+#   sudo cp -f files/usr/share/cinnamon/cinnamon-settings-users/cinnamon-settings-users.py /usr/share/cinnamon/cinnamon-settings-users/
 else
    echo "Cannot patch Cinnamon as the version is different than expected by the patcher"
 fi
